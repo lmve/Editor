99799e717d15737aff028aa2c24fe45f1768ddb4
lm <1831232566@qq.com>
2024-09-03 22:57:59 +0800

    read from user && process

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::io::{self, Read, stdout};
+use termion::raw::IntoRawMode;
+
+
+// quit the editor
+fn quit_editor(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b001_1111
+}
+
+// handle errors
+fn handle_errors(e: std::io::Error) {
+    panic!("{}", e);
+}
+
+fn main(){
+    // into_raw_mode() termion privides stdout.
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    // control chars are readable
+    for b in io::stdin().bytes(){
+        match b {
+            Ok(b) => {
+                let val = b;
+                let c = val as char;
+
+                if c.is_control() {
+                    println!("{:?} \r", val);
+                } else {
+                    println!("{:?} ({}) \r", val, c);
+                }
+
+                if val == quit_editor('q') {
+                    break;
+                }
+            },
+
+            Err(e) => handle_errors(e),
+        }
+    }
 }
